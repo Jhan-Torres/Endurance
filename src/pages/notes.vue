12cfa2183730e664b46fc126c958378a06587547
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-01-19 14:06:58 -0500

    feat: delete and edit buttons added

--- src/pages/notes.vue
+++ src/pages/notes.vue
@@ -28,10 +28,13 @@
     </fwb-button>
     <div class="bg-gray-400 py-4 px-6 rounded-lg flex flex-col items-center justify-center m-2 md:py-8 md:px-12 lg:px-14"
       v-if="formButton">
-      <fwb-input required placeholder="Title (*)" class="rounded-lg w-52 md:w-96 font-body" v-model="noteTitle"
-        v-bind:validation-status="validation">
+      <fwb-input required placeholder="Title (60 max)" class="rounded-lg w-52 md:w-96 font-body" v-model="noteTitle"
+        v-bind:validation-status="validationTitle">
         <template #validationMessage v-if="!noteTitle">
           Title required
+        </template>
+        <template #validationMessage v-if="noteTitle.length >= 60">
+          60 max capacity
         </template>
       </fwb-input>
       <fwb-input placeholder="Content" class="rounded-lg w-52 md:w-96 font-body my-2" v-model="noteContent" />
@@ -47,20 +50,37 @@
   </div>
   <div class="bg-slate-400 p-1 m-1 rounded-xl flex flex-wrap items-center justify-evenly gap-1 md:gap-3"
     v-if="notesList.length">
+    <!-- Loading element -->
+    <div role="status" class="hidden">
+      <svg aria-hidden="true" class="inline w-10 h-10text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor" />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill" />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
     <fwb-card class="w-80" v-for="note in notesList" :key="note.id">
-      <div class="p-2 font-body">
-        <h5 class="p-1 text-sm font-bold tracking-wider uppercase rounded-sm bg-gray-700 text-lime-50 md:text-lg">
+      <div class="p-1.5 font-body">
+        <h5 class="p-1 h-14 text-sm font-medium tracking-tight rounded-sm bg-gray-700 text-lime-50 md:text-base">
           {{ note.title }}
         </h5>
-        <hr class="border-gray-200 mx-auto my-2">
+        <hr class="border-gray-400 mx-auto my-1">
         <p class="text-xs text-gray-700 dark:text-gray-400 overflow-auto text-wrap h-20 md:text-sm">
           {{ note.content }}
         </p>
-        <hr class="border-gray-200 mx-auto mt-2">
-        <font-awesome-icon :icon="['fas', 'trash']"
-          class="hover:scale-125 hover:cursor-pointer mr-1 transition duration-200 trashIcon" />
-        <font-awesome-icon :icon="['fas', 'file-pen']"
-          class="hover:scale-125 hover:cursor-pointer ml-1 transition duration-200 editIcon" />
+        <hr class="border-gray-400 mx-auto mt-2">
+        <button class="trashIcon hover:scale-110 hover:cursor-pointer transition duration-200 mr-1"
+          @click="deleteButton(note.id)">
+          <font-awesome-icon :icon="['fas', 'trash']" />
+        </button>
+        <button class="editIcon hover:scale-110 hover:cursor-pointer transition duration-200 ml-1"
+          @click="editButton(note.id)">
+          <font-awesome-icon :icon="['fas', 'pen']" />
+        </button>
         <h6 class="text-sm font-semibold text-teal-500 inline-block float-right">{{ note.category }}</h6>
       </div>
     </fwb-card>
@@ -93,21 +113,24 @@ const optionsCategories = [
 ]
 
 //to show error message on required title accord to flowbite library
-let validation = ref('');
+let validationTitle = ref('');
 
 //to show "note added" notification
 let showAlert = ref(true);
 
+//to store note objects in array 
 const notesList = ref([]);
 
+//to set form button
 function toggleFormButton() {
   formButton.value = !formButton.value;
   if (!formButton.value) cleanInputs();
 }
 
+//to set button for add note
 function addNote() {
-  if (!noteTitle.value) {
-    validation.value = "error";
+  if (!noteTitle.value || noteTitle.value.length >= 60) {
+    validationTitle.value = "error";
     return;
   }
 
@@ -141,7 +164,7 @@ function showAddedNotification() {
 
 const cleanInputs = () => {
   noteTitle.value = '';
-  validation.value = '';
+  validationTitle.value = '';
   noteContent.value = '';
   noteCategory.value = '';
 }
