735cfa3e301bdf4c554e2c09fd36edbaa74f879b
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-02-02 23:10:53 -0500

    books form finished

--- src/components/books/Form.vue
+++ src/components/books/Form.vue
@@ -4,34 +4,37 @@
     color="dark"
     @click="toggleFormButton"
   >
-    <span>New Book</span>
+    New Book
   </fwb-button>
 
   <div 
-    class="flex flex-col items-center gap-1 justify-center mb-1 bg-slate-400 py-1 px-2 rounded-lg animate-fade-down animate-once animate-duration-500"
+    class="flex flex-col items-center gap-1 justify-center mb-1 bg-slate-400 py-1 px-2 rounded-lg animate-fade-down duration-500 md:py-2 md:px-5 lg:py-3 lg:px-7"
     v-if="showForm"
     v-on:keyup.esc="showForm = false"
-  >
+  > 
     <input 
+      id="title"
       type="text" 
       placeholder="Book Title (*)" 
       class="rounded-md w-52 text-xs" 
       v-model="bookObject.title" 
       ref="title"
     >
+    <label 
+      for="title" 
+      class="text-xs text-red-600" 
+      v-if="textError"
+    >
+      {{ textError }}
+    </label>
     <input 
       type="text" 
       placeholder="Autor" 
       class="rounded-md w-52 text-xs" 
       v-model="bookObject.autor"
     >
-    <div class="flex items-center">
-      <label 
-        for="select" 
-        class="text-xs mr-2"
-      >
-        Category
-      </label>
+    <div class="flex items-center gap-1">
+      <label for="select" class="text-xs mr-1">Category</label>
       <select 
         id="select" 
         class="rounded-md w-36 text-xs" 
@@ -52,23 +55,22 @@
       class="rounded-md w-52 text-xs" 
       v-model="bookObject.link"
     >
-
-    <fwb-button 
+    <div class="flex items-center gap-1">
+      <fwb-button 
         gradient="green-blue" 
         square 
         @click="createBook"
       >
         <span class="text-xs">Add Book</span>
       </fwb-button>
-
-    <fwb-button
-      color="red"
-      @click="showForm = false"
-    >
-      <span class="text-xs">Cancel</span>
-    </fwb-button>
+      <fwb-button
+        color="red"
+        @click="showForm = false"
+      >
+        <span class="text-xs">Cancel</span>
+      </fwb-button>
+    </div>
   </div>
-  
 </template>
 
 <script setup>
@@ -76,6 +78,8 @@ import { FwbButton } from 'flowbite-vue';
 import { ref, nextTick, onBeforeMount } from 'vue';
 
 let bookId;
+
+const textError = ref('');
 
 //Set note id at the begginig 
 onBeforeMount(() => {
@@ -90,27 +94,43 @@ const showForm = ref(false);
 const booksCategories = [
   'Astronomy',
   'Programming',
+  'Psychology',
   'Science'
 ]
 
 //html elements references
 const title = ref(null);
 
+//Emits & Props
+const emits = defineEmits(["addBookToList", "showAlert"]);
+
+const props = defineProps({
+
+})
+
 //METHODS
 function toggleFormButton() {
-  showForm.value = true;
+  showForm.value = !showForm.value;
 
   nextTick(() => {
     if (showForm.value) {
-      title.value.focus()
+      title.value.focus();
+      cleanInputs();
     }
   });
 }
 
 function createBook() {
-  if (bookObject.value.title) {
-    
-  }
+  if (!validateTitle()) return;
+  
+  bookObject.value.id = ++bookId;
+
+  localStorage.setItem('bookId', JSON.stringify(bookId));
+  showForm.value = false;
+
+  emits("addBookToList", bookObject.value);
+  emits("showAlert", 'book added');
+  console.log(bookObject.value);
 }
 
 function validateTitle() {
@@ -122,13 +142,18 @@ function validateTitle() {
   }
 
   //max 60 caracteres validation 
-  if (inputTitle.value.length >= 60) {
-    textError.value = "60 Max Capacity";
+  if (bookObject.value.title.length >= 30) {
+    textError.value = "30 Max Capacity";
     title.value.focus();
     return false;
   }
 
   return true;
+}
+
+function cleanInputs() {
+  bookObject.value = {};
+  textError.value = '';
 }
 
 </script>
