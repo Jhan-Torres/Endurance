b354445780192a4e6b566377d74b87e2901898d0
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-02-02 23:38:50 -0500

    feat: table component renamed to TableRow to use v-for cycle

---
+++ src/components/books/TableRow.vue
@@ -0,0 +1,35 @@
+<template>
+  <fwb-table-row draggable="true">
+    <fwb-table-cell class="text-xs font-thin md:text-sm">
+      Apple MacBook Pro 17"
+    </fwb-table-cell>
+    <fwb-table-cell class="text-xs font-thin md:text-sm">
+      Sliver
+    </fwb-table-cell>
+    <fwb-table-cell class="text-xs font-thin md:text-sm">
+      Laptop
+    </fwb-table-cell>
+    <fwb-table-cell class="text-xs font-thin md:text-sm">
+      $2999
+    </fwb-table-cell>
+    <fwb-table-cell class="text-xs font-thin md:text-sm">
+      <button class="hover:scale-125 hover:cursor-pointer transition duration-200 mr-0.5"> 
+        <font-awesome-icon :icon="['fas', 'trash']" :style="{color: 'rgb(190, 11, 11)'}"/>
+      </button>
+      <button class="hover:scale-125 hover:cursor-pointer transition duration-200 ml-0.5" >
+        <font-awesome-icon :icon="['fas', 'pen']" :style="{color: 'rgb(15, 15, 163)'}"/>
+      </button>
+    </fwb-table-cell>
+  </fwb-table-row>
+</template>
+
+<script setup>
+import {
+  FwbTableCell,
+  FwbTableRow,
+} from 'flowbite-vue'
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/books.vue
+++ src/pages/books.vue
@@ -3,13 +3,49 @@
     <Form/>
   </div>
   <div class="mx-3 mb-2">
-    <Table />
+    <fwb-table 
+      hoverable 
+      class="font-body animate-fade-up duration-500 max-w-screen-xl mx-auto"
+    >
+      <fwb-table-head class="bg-slate-700 text-white tracking-wider">
+        <fwb-table-head-cell>
+          <span class="text-xs font-thin md:text-sm">Title</span>
+        </fwb-table-head-cell>
+        <fwb-table-head-cell>
+          <span class="text-xs font-thin md:text-sm">Autor</span>
+        </fwb-table-head-cell>
+        <fwb-table-head-cell>
+          <span class="text-xs font-thin md:text-sm">Category</span>
+        </fwb-table-head-cell>
+        <fwb-table-head-cell>
+          <span class="text-xs font-thin md:text-sm">Link</span>
+        </fwb-table-head-cell>
+        <fwb-table-head-cell>
+          <span class="sr-only">Edit</span>
+        </fwb-table-head-cell>
+      </fwb-table-head>
+      <fwb-table-body>
+        <TableRow 
+          
+        />
+      </fwb-table-body>
+    </fwb-table>
   </div>
 </template>
 
 <script setup>
-import Table from '@/components/books/Table.vue';
+import TableRow from '@/components/books/TableRow.vue';
 import Form from '@/components/books/Form.vue';
+import {
+  FwbTable,
+  FwbTableBody,
+  FwbTableHead,
+  FwbTableHeadCell,
+} from 'flowbite-vue'
+import { ref } from 'vue';
+
+//Data refs
+const booksList = ref([]);
 </script>
 
 <style scoped>
