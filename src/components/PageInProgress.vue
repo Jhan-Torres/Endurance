3379fce263150926f40ad449dab61c4661fcdb9f
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-03-02 19:10:01 -0500

    feat: tailwind text shadow style added

--- src/components/PageInProgress.vue
+++ src/components/PageInProgress.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="flex flex-col justify-center items-center p-2">
-    <h1 class="font-body text-3xl uppercase font-bold">
-      [ <span class="text-[#FF1F00]">{{ props.pageName }} page</span> in progress ]
+    <h1 class="font-body text-3xl uppercase font-bold text-center mb-2 text-shadow shadow-blue-600">
+      [<span class="text-[#FF1F00]">{{ props.pageName }} page</span> in progress]
     </h1>
-    <img src="../../public/img/Landing_page_animation.gif" alt="gif by Storyset" class="rounded-3xl">
-    <a href="https://storyset.com/work" class="italic underline">Work illustrations by Storyset</a>
+    <div>
+      <img 
+        src="/img/Landing_page_animation.gif" 
+        alt="gif by Storyset" 
+        class="rounded-3xl h-[50vh]"
+      >
+    </div>
+    <a 
+      href="https://storyset.com/work" 
+      class="italic underline"
+    >
+      Work illustrations by Storyset
+    </a>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./index.html",
@@ -21,11 +23,26 @@ export default {
           '50%': { backgroundPosition: '100% 50%' },
         },
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      }
     },
   },
   plugins: [
     require('flowbite/plugin'),
-    require('tailwindcss-animated')
+    require('tailwindcss-animated'),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
   ],
 }
 
