6b3e0398b761a4ccb81bd5487549719f16fad752
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-03-13 23:36:10 -0500

    login button colors modified according to logIn/Out

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -1,12 +1,26 @@
 <template>
   <div
     @click="handleSession"
-    class="font-body text-center group relative inline-block overflow-hidden rounded border-4 bg-gray-300 px-6 py-1 font-bold text-slate-800 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white cursor-pointer"
+    class="font-body text-center group relative inline-block overflow-hidden rounded border-4 bg-gray-300 px-6 py-1 font-bold text-slate-800 focus:outline-none focus:ring active:text-white cursor-pointer"
+    :class="{'hover:text-violet-600 active:bg-indigo-600' : buttonText === 'LogIn', 
+    'hover:text-red-500 active:bg-red-400' : buttonText === 'LogOut'}"
   >
-    <span class="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-violet-600 transition-all duration-200 group-hover:w-full" />
-    <span class="ease absolute right-0 top-0 h-0 w-0 border-r-4 border-violet-600 transition-all duration-200 group-hover:h-full" />
-    <span class="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-violet-600 transition-all duration-200 group-hover:w-full" />
-    <span class="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-violet-600 transition-all duration-200 group-hover:h-full" />
+    <span 
+      class="ease absolute left-0 top-0 h-0 w-0 border-t-2  transition-all duration-200 group-hover:w-full" 
+      :class="{'border-violet-600' : buttonText === 'LogIn', 'border-red-500' : buttonText === 'LogOut'}"
+    />
+    <span 
+      class="ease absolute right-0 top-0 h-0 w-0 border-r-4 transition-all duration-200 group-hover:h-full" 
+      :class="{'border-violet-600' : buttonText === 'LogIn', 'border-red-500' : buttonText === 'LogOut'}"
+    />
+    <span 
+      class="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 transition-all duration-200 group-hover:w-full" 
+      :class="{'border-violet-600' : buttonText === 'LogIn', 'border-red-500' : buttonText === 'LogOut'}"
+    />
+    <span 
+      class="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 transition-all duration-200 group-hover:h-full" 
+      :class="{'border-violet-600' : buttonText === 'LogIn', 'border-red-500' : buttonText === 'LogOut'}"
+    />
       {{ buttonText }}
 </div>
 </template>
@@ -23,7 +37,7 @@ const buttonText = computed(() => {
 
 function handleSession() {
   if (buttonText.value === 'LogIn') {
-    router.push('/auth')
+    router.replace({name : 'auth'})
   } else {
     useFirebaseAuth().userLogout();
   }
